7da64736ca89f5dc8a10e097c4343b19a7a4df6f
JoKeR31327 <shadinarifulislam@gmail.com>
2026-08-25 00:12:24 +0600

    cleared up

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-English Handwritten Exam OCR & Confidence Research Subsystem.
-"""
-
-__version__ = "1.0.0"
--- src/preprocessing/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Image Preprocessing Package.
-"""
-
-from src.preprocessing.image import ImagePreprocessor, safe_normalize_text
-
-__all__ = ["ImagePreprocessor", "safe_normalize_text"]
